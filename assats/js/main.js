afb1c87f19b352c8fe14bfba2cb7af4e0669df8e
semen <shaposhnik.semen@yendex.ru>
2024-12-15 18:44:33 +0500

    Добавил панель с иконками и доработал пару стили, хотел ещё сделать новую кнопку с изминениеем light/dark но сложно чето

--- assats/js/main.js
+++ assats/js/main.js
@@ -1,3 +1,19 @@
+// тёмный/светлый режим страницы 
+$(document).ready(function(){
+    $("#tooglenight").change(function() {
+        if(this.checked) {
+            $("#bd").addClass("night");
+            $("i").css("color", "white");
+        }else{
+            $("#bd").removeClass("night");
+            $("i").css("color", "black");
+        }
+    });
+});
+// тёмный/светлый режим страницы 
+
+
+
 document.addEventListener('DOMContentLoaded', function() {
     const searchForm = document.querySelector('form');
     const searchInput = document.querySelector('input[name="s"]');
